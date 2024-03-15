cb887f87c7f41c559fed15adc8d900cd39c42aa9
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-03-15 21:31:35 +0300

    currency-convertor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import Block from "./components/Block";
+import "./style/style.scss"
 
 function App() {
+
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const[fromPrice, setFromPrice] = useState();
+  const[toPrice, setToPrice] = useState(1);
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+    .then((resolve) => resolve.json())
+    .then((json) => {
+      setRates(json.Valute);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert("Не удалось получить информацию!");
+    });
+  }, []); 
+
+
+  const onChangeFromPrice = (value) => {
+    let price = 0;
+    let result = 0;
+    if(fromCurrency === "RUB"){
+      if(toCurrency !== "RUB"){
+        price = value;
+        result = price / rates[toCurrency]['Value']
+      }else{
+        price = value;
+        result = value;
+      }
+    }else{
+      if(toCurrency !== "RUB"){
+        price = value;
+        result = price * rates[toCurrency]['Value'] / rates[fromCurrency]['Value'];
+      }else{
+        price = value;
+        result = price * rates[fromCurrency]['Value'];
+      }
+      
+    }
+    setToPrice(result);
+    setFromPrice(value);
+  }
+
+
+  const onChangeToPrice = (value) => {
+    let price = 0;
+    let result = 0;
+    if(toCurrency === "RUB"){
+      if(fromCurrency !== "RUB"){
+        price = value;
+        result = price / rates[fromCurrency]["Value"]
+      }else{
+        price = value;
+        result = value;
+      }
+    }else{
+      if(fromCurrency !== "RUB"){
+        price = value;
+        result = price * rates[fromCurrency]["Value"] / rates[toCurrency]["Value"];
+      }else{
+        price = value;
+        result = price * rates[toCurrency]["Value"];
+      }
+      
+    }
+    setFromPrice(result);
+    setToPrice(value);
+  }
+
+//   useEffect(() => {
+//     onChangeFromPrice(fromPrice);
+// }, [fromCurrency, fromPrice, onChangeFromPrice]);
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
